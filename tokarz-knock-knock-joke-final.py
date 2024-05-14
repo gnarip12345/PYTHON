8620a0793689d265257557bde5e0992e38420271
gnarip12345 <125836380+gnarip12345@users.noreply.github.com>
2024-05-14 16:46:52 -0500

    Add files via upload

---
+++ tokarz-knock-knock-joke-final.py
@@ -0,0 +1,9 @@
+# Knock Knock Joke Program
+print("Knock, Knock")
+print("Who's There?")
+print("Ducks.")
+print("Ducks who?")
+print("Ducks are everywhere in the class modules!")
+
+# As one line of code using escape characters
+print("Knock, Knock.\nWho's there?\nDucks.\nDucks who?\nDucks are everywhere in the class modules!")
