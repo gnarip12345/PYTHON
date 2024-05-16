0b039ecb78b24edbc2d29a1caa34dda84ecc5223
gnarip12345 <125836380+gnarip12345@users.noreply.github.com>
2024-05-16 16:44:18 -0500

    Add files via upload
    mathematical operations - Week 2 - Jon Tokarz

---
+++ mathematical-operations-tokarz.py
@@ -0,0 +1,37 @@
+# Mathematical Operations
+
+# Addition
+sum = 5 + 5
+print("\nAddition: ")
+print(sum)
+
+# Subtraction
+diff = 9 - 5
+print("\nSubtraction: ")
+print(diff)
+
+# Multiplication
+product = 200 * 500
+print("\nMultiplication: ")
+print(product)
+
+# Division
+quotient = 55 / 11
+print("\nDivision: ")
+print(quotient)
+
+# Integer Division (Floor Division)
+integer_div = 13 // 6
+print("\nInteger Division: ")
+print(integer_div)
+
+# Modulus 
+remainder = 99 % 4
+print("\nModulus: ")
+print(remainder)
+
+# Exponentiation
+exp = 2 ** 3
+print("\nExponentiation: ")
+print(exp)
+
