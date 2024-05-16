7bd5857703cac70a043ea6adaf1e7c741ff805c1
gnarip12345 <125836380+gnarip12345@users.noreply.github.com>
2024-05-16 16:17:15 -0500

    Add files via upload
    variable types assignment - Week 2

---
+++ variable-types-tokarz.py
@@ -0,0 +1,23 @@
+# Python can demonstrate different data types
+
+# Literal
+x = 25
+print("Literal:" , x)
+
+# Integer
+y = -200
+print("Integer:" , y)
+
+# Float
+z = 29.5
+print("Float:" , z)
+
+# String
+name = "Ciera"
+print("String:" , name)
+
+# Boolean
+is_active = True
+print("Boolean:" , is_active)
+
+
