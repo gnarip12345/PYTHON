e885df9c73c637a9d67699dfe3d734b5523cd6c8
gnarip12345 <125836380+gnarip12345@users.noreply.github.com>
2024-05-18 11:47:06 -0500

    Add files via upload
    Kilograms to Pounds conversion code

---
+++ Jon_Tokarz_2.4_Variables_and_Formatting_Numbers.py
@@ -0,0 +1,34 @@
+# Original values in Kilograms
+kg_value_1 = 40
+kg_value_2 = 65
+kg_value_3 = 700
+kg_value_4 = 1700
+
+# Conversion value Kilograms to Pounds
+kilograms_to_pounds = 2.20462
+
+# Calculate each Kilograms to Pounds
+pounds_value_1 = kg_value_1 * kilograms_to_pounds
+pounds_value_2 = kg_value_2 * kilograms_to_pounds
+pounds_value_3 = kg_value_3 * kilograms_to_pounds
+pounds_value_4 = kg_value_4 * kilograms_to_pounds
+
+# Output the results
+print(f"{kg_value_1} kilograms is equal to {pounds_value_1} pounds.")
+print(f"{kg_value_2} kilograms is equal to {pounds_value_2} pounds.")
+print(f"{kg_value_3} kilograms is equal to {pounds_value_3} pounds.")
+print(f"{kg_value_4} kilograms is equal to {pounds_value_4} pounds.")
+
+
+
+
+
+# Jon Tokarz May 18, 2024
+
+
+
+
+
+
+
+
