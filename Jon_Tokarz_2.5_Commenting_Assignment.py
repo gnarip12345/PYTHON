e3c2c6b14174dea19e61d396db1b52f6aed14606
gnarip12345 <125836380+gnarip12345@users.noreply.github.com>
2024-05-18 13:32:30 -0500

    Add files via upload
    Jon Tokarz 2.5 Assignment - Commenting Code

---
+++ Jon_Tokarz_2.5_Commenting_Assignment.py
@@ -0,0 +1,16 @@
+#Tank Size
+tank_size = 15
+
+#Distance to be traveled 
+distance = 600
+
+#Miles per gallon calculation
+mpg = distance / tank_size
+
+#Display of miles per gallon
+print("Miles per gallon:", mpg)
+
+
+
+
+#Jon Tokarz 2.5 Assignment - May 18, 2024
