03e7d4f64c8fa422870d00499e080e2e3e70833a
gnarip12345 <125836380+gnarip12345@users.noreply.github.com>
2024-06-15 15:15:24 -0500

    Add files via upload
    4.2 - Mad Lib Song - Jingle Bells - Jon Tokarz - Assignment

---
+++ 4.2 - Jingle Bells Madlib - Jon Tokarz - Assignment.py
@@ -0,0 +1,63 @@
+# 4.2 - Jingle Bells Madlib - Jon Tokarz - Assignment - June 15, 2024
+
+# Inputs for the various lines of the song "Jingle Bells"
+adjective_1 = input("Adjective: ")
+plural_noun1 = input("Plural Noun: ")
+verb_1 = input("Verb: ") 
+adjective_2 = input("Adjective: ")
+adjective_3 = input("Adjective: ")
+noun_1 = input("Noun: ")
+verbendingining_1 = input('Verb Ending In "ing": ')
+place_1 = input("Noun: ")
+pluralnoun_2 = input("Plural Noun: ")
+verb_2 = input("Verb: ")
+verbendingining_2 = input('Verb Ending in "ing": ')
+plural_noun3 = input("Plural Noun: ")
+noun_noun = input("Noun: ")
+verb_3 = input("Verb: ")
+pluralnoun_4 = input("Plural Noun: ")
+adjective_4 = input("Adjective: ")
+verb_4 = input("Verb: ")
+verb_5 = input("Verb: ")
+adjective_5 = input("Adjective: ")
+noun_3 = input("Noun: ")
+adjective_extra = input("Adjective: ")
+
+# Printing the song as a function
+def jingle_bells():
+    print("\n")
+    print(adjective_1, plural_noun1)
+    print(adjective_1, plural_noun1)
+    print(adjective_1, "all the way,")
+    print("oh what fun it is to", verb_1)
+    print("in a", adjective_2, adjective_3, noun_1,"! Hey!\n")
+
+    print(adjective_1, plural_noun1)
+    print(adjective_1, plural_noun1)
+    print(adjective_1, "all the way,")
+    print("oh what fun it is to", verb_1)
+    print("in a", adjective_2, adjective_3, noun_1,"! Hey!\n")
+
+    print(verbendingining_1, "through the", place_1)
+    print("in a", adjective_2, adjective_3, noun_1)
+    print("over the", pluralnoun_2, "we", verb_2)
+    print(verbendingining_2, "all the way! Ha ha ha!\n")
+
+    print(plural_noun3, "on", noun_noun, verb_3)
+    print("Making", pluralnoun_4, adjective_4)
+    print("What fun it is to", verb_4, "and", verb_5)
+    print("A", adjective_5, noun_3, adjective_extra, "!\n")
+
+    print(adjective_1, plural_noun1)
+    print(adjective_1, plural_noun1)
+    print(adjective_1, "all the way,")
+    print("oh what fun it is to", verb_1)
+    print("in a", adjective_2, adjective_3, noun_1,"! Hey!\n")
+
+jingle_bells()
+
+
+
+
+
+# 4.2 - Jingle Bells Madlib - Jon Tokarz - Assignment - June 15, 2024
