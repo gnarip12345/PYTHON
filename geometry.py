d035ec670cdba91d7e697507ff78a0776598a90a
gnarip12345 <125836380+gnarip12345@users.noreply.github.com>
2024-06-28 14:49:02 -0500

    Add files via upload
    1.3 - Geometry Package - Jon Tokarz

---
+++ geometry.py
@@ -0,0 +1,15 @@
+# Functions for finding areas of various euclidian geometry:
+
+def rectangle(h, w)
+    area_rectangle = (h * w)
+    return area_rectangle
+
+def triangle(h, w)
+    area_triangle = (.5 * h * w)
+    return area_triangle
+
+def circle(r)
+    area_circle = (3.14 * (r * r))
+    return area_circle
+
+# Jon Tokarz - June 28, 2024
---
+++ main.py
@@ -0,0 +1,11 @@
+from math_operations import calculator
+
+# rest of your code...
+
+import calculator
+
+result = calculator.add(5, 3)
+print("Addition result:", result)
+
+result = calculator.subtract(10, 4)
+print("Subtraction result:", result)
