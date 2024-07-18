a07045a4cec3d7846a653d8638d5e5359b8bd58b
gnarip12345 <125836380+gnarip12345@users.noreply.github.com>
2024-07-18 11:13:02 -0500

    Add files via upload
    4.1 - Generators, Iterators, and Closures - Jon Tokarz

---
+++ 4.1_Generators_Iterators_Closures_Jon_Tokarz.py
@@ -0,0 +1,35 @@
+# 4.1 - Generators, Iterators, and Closures - Jon Tokarz - July 18, 2024
+
+
+# Define a generator function two_letter_combinations that takes a list of characters as an argument
+# Use nested loops within the generator function to iterate of the list of characters.
+def two_letter_combinations(characters):
+
+    for char1 in characters:
+        for char2 in characters:
+
+            combo = char1 + char2
+
+            yield combo
+
+# Run if the script is being run as the main program
+if __name__ == "__main__":
+
+    # A sample list of characters
+    char_list = ['a', 'b', 'c']
+
+    # Using the generator function for the char_list
+    combinations = two_letter_combinations(char_list)
+
+    # Print the combo yield from the two_letter_combinations function
+    for combo in combinations:
+        print(combo)
+
+
+
+
+
+
+
+
+# 4.1 - Generators, Iterators, and Closures - Jon Tokarz - July 18, 2024
