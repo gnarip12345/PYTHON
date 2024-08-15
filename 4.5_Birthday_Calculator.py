fbc0b22236a863e8586b27c188999d1a412a6133
gnarip12345 <125836380+gnarip12345@users.noreply.github.com>
2024-08-15 13:43:25 -0500

    Update 4.5_Birthday_Calculator.py
    Updated - calculations included for birthdate in months, weeks, and days. _JT

--- 4.5_Birthday_Calculator.py
+++ 4.5_Birthday_Calculator.py
@@ -10,16 +10,20 @@ def main():
     try:
         # Importing current date for calculating age
         today = datetime.today()
+       
         # Input for birth-year
         birth_year = int(input("What year were you born?  "))
+       
         # Input for birth- month
         month = int(input("What Month were you born (as a number. May would be 5)  "))
+       
         # Input for birth-day
         day = int(input("What day of the month were you born?  "))
 
         # just need datetime not datetime.date
 
         # Calculating age from input and current datetime
+
         # because we imported datetime from datetime
         birthday = datetime(birth_year, month, day)
         print("Your birthday is: ")
@@ -31,6 +35,22 @@ def main():
         delta_years = delta.days // 365.2425
        
         print(f'You are {delta_years} years old')
+
+        # Calculate the months from the years
+        delta_months = delta_years * 12
+
+        # Calculate the weeks from the days
+        delta_weeks = delta.days // 7
+
+        # Display the age in months
+        print(f"You are {delta_months} months old.")
+        # Display the age in weeks
+        print(f"You are {delta_weeks} weeks old.")
+        # Display the age in days
+        print(f"You are {delta.days} days old.")
+
+
+
        
       
     except Exception as e:
@@ -43,4 +63,6 @@ main()
 
 
 
-# 4.5 - Birthday Calculator - July 24, 2024
+# 4.5 - Birthday Calculator - July 24, 2024        
+
+
